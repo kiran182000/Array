921ebd713908564556551305b60fcf1c601ac31d
kiran <kiran405411@gmail.com>
2024-02-05 15:56:36 +0530

    Sorting

--- src/array.java
+++ src/array.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 public class array {
     public static void main(String[] args){
         int[] arr={1,2,3,4,5};
-        for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+        Arrays.sort(arr);
+        for(int i:arr)
+        System.out.print(i + " ");
         }
-    }
 }
+

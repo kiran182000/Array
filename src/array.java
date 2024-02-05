69b521b76840a80b8bd0bbe957bacdec7129792a
kiran <kiran405411@gmail.com>
2024-02-05 15:51:49 +0530

    Duplicate elements

--- src/array.java
+++ src/array.java
@@ -1,8 +1,11 @@
 public class array {
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
+        int[] arr={1,2,3,4,5,2,4};
         for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+            for(int j=i+1;j<arr.length;j++) {
+                if (arr[i] == arr[j])
+                    System.out.print(arr[j] + " ");
+            }
         }
     }
 }

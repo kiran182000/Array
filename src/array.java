2c15e381309cd83efc592f64f979b495a46595f7
kiran <kiran405411@gmail.com>
2024-02-05 15:40:14 +0530

    Even position

--- src/array.java
+++ src/array.java
@@ -1,8 +1,8 @@
 public class array {
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+        int[] arr={0,1,2,3,4,5};
+        for(int i=0;i< arr.length;i+=2){
+            System.out.println("Elements at even position are: " +arr[i]);
         }
     }
 }

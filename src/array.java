52b0f0ab91a6fbf635461aab6cf02c3cd924fbe9
kiran <kiran405411@gmail.com>
2024-02-05 15:38:06 +0530

    Smallest Element

--- src/array.java
+++ src/array.java
@@ -1,8 +1,11 @@
 public class array {
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+        int[] arr={4,2,3,1,5};
+        int small=arr[0];
+        for(int i=1;i< arr.length;i++) {
+            if (arr[i] < arr[0])
+                small=arr[i];
         }
+        System.out.print("Smallest element: " +small);
     }
 }

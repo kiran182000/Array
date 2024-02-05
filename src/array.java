8416764bd724857332c74d2db08402e1af48aaf2
kiran <kiran405411@gmail.com>
2024-02-05 15:29:02 +0530

    Frequency checking

--- src/array.java
+++ src/array.java
@@ -1,8 +1,20 @@
+import java.util.HashMap;
+
 public class array {
-    public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 2, 3, 1, 2, 4, 5, 3, 2};
+
+        HashMap<Integer, Integer> frequency = new HashMap<>();
+        for (int i : array) {
+            if (frequency.containsKey(i)) {
+                frequency.put(i, frequency.get(i) + 1);
+            } else {
+                frequency.put(i, 1);
+            }
+        }
+        for (int key : frequency.keySet()) {
+            System.out.println("Element " + key + " occurs " + frequency.get(key) + " times");
         }
     }
 }
+

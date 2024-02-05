42309f6e39e5b1f21285261976d7f37f46282d86
kiran <kiran405411@gmail.com>
2024-02-05 15:33:56 +0530

    Largest element

--- src/array.java
+++ src/array.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 public class array {
     public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+        int[] arr={1,2,5,4,3};
+        Arrays.sort(arr);
+        System.out.println("Largest elements is: "+ arr[arr.length-1]);
         }
     }
-}
+

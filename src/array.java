ab680f5c5d2bf86d5c6f6926011af270a27e0748
kiran <kiran405411@gmail.com>
2024-02-05 15:59:37 +0530

    Second largest

--- src/array.java
+++ src/array.java
@@ -1,8 +1,8 @@
+import java.util.Arrays;
 public class array {
-    public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
-        for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
-        }
+    public static void main(String[] args) {
+        int[] arr = {8, 2, 4, 9, 4, 6};
+        Arrays.sort(arr);
+        System.out.print("Second largest element is: " + arr[arr.length-2]);
     }
 }

8de2d29a11860d82839cc788b496f4b25fb88374
kiran <kiran405411@gmail.com>
2024-02-05 15:24:48 +0530

    printing elements

--- src/array.java
+++ src/array.java
@@ -2,7 +2,7 @@ public class array {
     public static void main(String[] args){
         int[] arr={1,2,3,4,5};
         for(int i=0;i< arr.length;i++){
-            System.out.println(arr[i]);
+            System.out.println("Elements of array are:" +arr[i]);
         }
     }
 }
